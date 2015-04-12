d46a124be3196b02266f3edb7f3a2e6763074736
Amnon Heiman <amnon@cloudius-systems.com>
2015-04-12 12:48:28 +0300

    Adding the MBean Service Support
    This adds the java project that expose the system MBean.
    The Main would start any required classes and would wait forever.
    
    The mvn creates jar with main and set the classpath accordingly.
    To start do:
    
    java -jar target/urchin-mbean-1.0.jar
    
    Signed-off-by: Amnon Heiman <amnon@cloudius-systems.com>

---
+++ src/main/java/com/cloudius/main/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2015 Cloudius Systems
+ */
+package com.cloudius.main;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Starting the JMX server");
+        Thread.sleep(Long.MAX_VALUE);
+    }
+
+}
