77eb357646a1972decf6da477a1eaf500eb3f7dd
Amnon Heiman <amnon@cloudius-systems.com>
2015-04-13 09:28:47 +0300

    APIClient a Json Client Stub
    The client hides the comunication details between the jmx and the REST
    API.
    
    The current stub was created so an MBean that uses it, can compile and
    run with no errors.
    
    Signed-off-by: Amnon Heiman <amnon@cloudius-systems.com>

--- src/main/java/com/cloudius/main/Main.java
+++ src/main/java/com/cloudius/main/Main.java
@@ -3,9 +3,12 @@
  */
 package com.cloudius.main;
 
+import com.cloudius.api.APIClient;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        System.out.println("Connecting to " + APIClient.getBaseUrl());
         System.out.println("Starting the JMX server");
         Thread.sleep(Long.MAX_VALUE);
     }
