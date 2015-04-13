c64b4b6e5694563ae322adb4633a06652c5b43ea
Amnon Heiman <amnon@cloudius-systems.com>
2015-04-13 09:30:10 +0300

    Adding the StorageServiceMBean stub
    This adds a stub of the StorageService.
    After applying it, it will be possible to use JConsole to connect to the
    process and run any of the command and inquire all the properties.
    
    Some deprecated methods uses RepairParallelism class, an empty
    implementation for it is added. It would be removed with future version
    of the MBean, when those methods will be removed from the MBean
    
    As this is a stub, the only effect is a printout on the screen.
    
    Signed-off-by: Amnon Heiman <amnon@cloudius-systems.com>

--- src/main/java/com/cloudius/main/Main.java
+++ src/main/java/com/cloudius/main/Main.java
@@ -4,12 +4,14 @@
 package com.cloudius.main;
 
 import com.cloudius.api.APIClient;
+import org.apache.cassandra.service.StorageService;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Connecting to " + APIClient.getBaseUrl());
         System.out.println("Starting the JMX server");
+        StorageService.getInstance();
         Thread.sleep(Long.MAX_VALUE);
     }
 
