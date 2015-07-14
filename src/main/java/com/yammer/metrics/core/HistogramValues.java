5e19158c0fb315ee911f7c9e301e747f5c91200d
Amnon Heiman <amnon@cloudius-systems.com>
2015-07-14 17:35:13 +0300

    Adding the HistogramValue object
    The HistogramValue object is used by the APIClient to returns a
    histogram object from the API.
    
    It contains the values that are defined in utils.json
    
    Signed-off-by: Amnon Heiman <amnon@cloudius-systems.com>

---
+++ src/main/java/com/yammer/metrics/core/HistogramValues.java
@@ -0,0 +1,11 @@
+package com.yammer.metrics.core;
+
+public class HistogramValues {
+    public long count;
+    public long min;
+    public long max;
+    public long sum;
+    public double variance;
+    public double svariance;
+    public long sample[];
+}
