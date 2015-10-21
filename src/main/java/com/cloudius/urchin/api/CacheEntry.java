a7c30493cd6445661d880f5f0b90dd46bbdd4b4f
Amnon Heiman <amnon@cloudius-systems.com>
2015-10-21 11:33:00 +0300

    APIClient to support EstimatedHistogram
    This patch modify the CacheEntry to support both String and
    EstimatedHistogram.
    
    It is possible to add more supported types in the future when needed.
    
    In the APIClient, the cache will now support both String and
    EstimatedHistogram in a similiar way.
    
    Signed-off-by: Amnon Heiman <amnon@cloudius-systems.com>
    
    apiclient need to merge to cache

--- src/main/java/com/cloudius/urchin/api/CacheEntry.java
+++ src/main/java/com/cloudius/urchin/api/CacheEntry.java
@@ -1,20 +1,46 @@
 /*
- * Copyright 2015 Cloudius Systems
+ * Copyright (C) 2015 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.cloudius.urchin.api;
 
+import com.cloudius.urchin.utils.EstimatedHistogram;
+
 public class CacheEntry {
     long time;
-    public String value;
+    Object value;
 
-    CacheEntry(String value) {
+    CacheEntry(Object res) {
         time = System.currentTimeMillis();
-        this.value = value;
+        this.value = res;
     }
 
     public boolean valid(long duration) {
         return (System.currentTimeMillis() - time) < duration;
     }
 
+    public String stringValue() {
+        return (String) value;
+    }
+
+    public EstimatedHistogram getEstimatedHistogram() {
+        return (EstimatedHistogram)value;
+    }
 }
