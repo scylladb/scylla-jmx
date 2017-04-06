scylladb/scylla-jmx

commit 3e146845b42cb6bafbcff336d70e3c846d5b1528

StorageService: update to c3 compat
Note: some calls that are not (yet) applicable to scylla are 
unimplemented.

src/main/java/org/apache/cassandra/service/StorageServiceMBean.java
@@ -15,6 +15,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/*
+ * Copyright 2015 Cloudius Systems
+ *
+ * Modified by Cloudius Systems
+ */
+
 package org.apache.cassandra.service;
 
 import java.io.IOException;
@@ -173,13 +180,11 @@ public interface StorageServiceMBean extends NotificationEmitter {
     /** Retrieve the mapping of endpoint to host ID */
     public Map<String, String> getHostIdMap();
 
-    /**
-     * Numeric load value.
-     *
-     * @see org.apache.cassandra.metrics.StorageMetrics#load
-     */
-    @Deprecated
-    public double getLoad();
+    /** Retrieve the mapping of endpoint to host ID */
+    public Map<String, String> getEndpointToHostId();
+
+    /** Retrieve the mapping of host ID to endpoint */
+    public Map<String, String> getHostIdToEndpoint();
 
     /** Human-readable load value */
     public String getLoadString();
@@ -223,6 +228,17 @@ public interface StorageServiceMBean extends NotificationEmitter {
      */
     public void takeSnapshot(String tag, String... keyspaceNames)
             throws IOException;
+
+    /**
+     * Takes the snapshot of a specific column family. A snapshot name must be specified.
+     *
+     * @param keyspaceName the keyspace which holds the specified column family
+     * @param tableName the table to snapshot
+     * @param tag the tag given to the snapshot; may not be null or empty
+     */
+    default void takeTableSnapshot(String keyspaceName, String tableName, String tag) throws IOException {
+        takeColumnFamilySnapshot(keyspaceName, tableName, tag);
+    }
 
     /**
      * Takes the snapshot of a specific column family. A snapshot name must be
@@ -273,18 +289,21 @@ public interface StorageServiceMBean extends NotificationEmitter {
     public long trueSnapshotsSize();
 
     /**
+     * Forces refresh of values stored in system.size_estimates of all column families.
+     */
+    public void refreshSizeEstimates() throws ExecutionException;
+
+    /**
      * Forces major compaction of a single keyspace
      */
-    public void forceKeyspaceCompaction(String keyspaceName,
-            String... columnFamilies) throws IOException, ExecutionException,
-                    InterruptedException;
+    public void forceKeyspaceCompaction(boolean splitOutput, String keyspaceName, String... tableNames) throws IOException, ExecutionException, InterruptedException;
 
     /**
      * Trigger a cleanup of keys on a single keyspace
      */
-    public int forceKeyspaceCleanup(String keyspaceName,
-            String... columnFamilies) throws IOException, ExecutionException,
-                    InterruptedException;
+    @Deprecated
+    public int forceKeyspaceCleanup(String keyspaceName, String... tables) throws IOException, ExecutionException, InterruptedException;
+    public int forceKeyspaceCleanup(int jobs, String keyspaceName, String... tables) throws IOException, ExecutionException, InterruptedException;
 
     /**
      * Scrub (deserialize + reserialize at the latest version, skipping bad rows
@@ -294,23 +313,26 @@ public interface StorageServiceMBean extends NotificationEmitter {
      * Scrubbed CFs will be snapshotted first, if disableSnapshot is false
      */
     @Deprecated
-    public int scrub(boolean disableSnapshot, boolean skipCorrupted,
-            String keyspaceName, String... columnFamilies) throws IOException,
-                    ExecutionException, InterruptedException;
+    public int scrub(boolean disableSnapshot, boolean skipCorrupted, String keyspaceName, String... tableNames) throws IOException, ExecutionException, InterruptedException;
+    @Deprecated
+    public int scrub(boolean disableSnapshot, boolean skipCorrupted, boolean checkData, String keyspaceName, String... tableNames) throws IOException, ExecutionException, InterruptedException;
+    public int scrub(boolean disableSnapshot, boolean skipCorrupted, boolean checkData, int jobs, String keyspaceName, String... columnFamilies) throws IOException, ExecutionException, InterruptedException;
 
-    public int scrub(boolean disableSnapshot, boolean skipCorrupted,
-            boolean checkData, String keyspaceName, String... columnFamilies)
-                    throws IOException, ExecutionException,
-                    InterruptedException;
+    /**
+     * Verify (checksums of) the given keyspace.
+     * If tableNames array is empty, all CFs are verified.
+     *
+     * The entire sstable will be read to ensure each cell validates if extendedVerify is true
+     */
+    public int verify(boolean extendedVerify, String keyspaceName, String... tableNames) throws IOException, ExecutionException, InterruptedException;
 
     /**
      * Rewrite all sstables to the latest version. Unlike scrub, it doesn't skip
      * bad rows and do not snapshot sstables first.
      */
-    public int upgradeSSTables(String keyspaceName,
-            boolean excludeCurrentVersion, String... columnFamilies)
-                    throws IOException, ExecutionException,
-                    InterruptedException;
+    @Deprecated
+    public int upgradeSSTables(String keyspaceName, boolean excludeCurrentVersion, String... tableNames) throws IOException, ExecutionException, InterruptedException;
+    public int upgradeSSTables(String keyspaceName, boolean excludeCurrentVersion, int jobs, String... tableNames) throws IOException, ExecutionException, InterruptedException;
 
     /**
      * Flush all memtables for the given column families, or all columnfamilies
@@ -325,70 +347,66 @@ public interface StorageServiceMBean extends NotificationEmitter {
                     InterruptedException;
 
     /**
-     * Invoke repair asynchronously. You can track repair progress by
-     * subscribing JMX notification sent from this StorageServiceMBean.
-     * Notification format is: type: "repair" userObject: int array of length 2,
-     * [0]=command number, [1]=ordinal of ActiveRepairService.Status
+     * Invoke repair asynchronously.
+     * You can track repair progress by subscribing JMX notification sent from this StorageServiceMBean.
+     * Notification format is:
+     *   type: "repair"
+     *   userObject: int array of length 2, [0]=command number, [1]=ordinal of ActiveRepairService.Status
      *
+     * @param keyspace Keyspace name to repair. Should not be null.
+     * @param options repair option.
      * @return Repair command number, or 0 if nothing to repair
      */
-    public int forceRepairAsync(String keyspace, boolean isSequential,
-            Collection<String> dataCenters, Collection<String> hosts,
-            boolean primaryRange, boolean repairedAt, String... columnFamilies)
-                    throws IOException;
+    public int repairAsync(String keyspace, Map<String, String> options);
 
     /**
-     * Invoke repair asynchronously. You can track repair progress by
-     * subscribing JMX notification sent from this StorageServiceMBean.
-     * Notification format is: type: "repair" userObject: int array of length 2,
-     * [0]=command number, [1]=ordinal of ActiveRepairService.Status
+     * @deprecated use {@link #repairAsync(String keyspace, Map options)} instead.
+     */
+    @Deprecated
+    public int forceRepairAsync(String keyspace, boolean isSequential, Collection<String> dataCenters, Collection<String> hosts,  boolean primaryRange, boolean fullRepair, String... tableNames) throws IOException;
+
+    /**
+     * Invoke repair asynchronously.
+     * You can track repair progress by subscribing JMX notification sent from this StorageServiceMBean.
+     * Notification format is:
+     *   type: "repair"
+     *   userObject: int array of length 2, [0]=command number, [1]=ordinal of ActiveRepairService.Status
      *
-     * @param parallelismDegree
-     *            0: sequential, 1: parallel, 2: DC parallel
+     * @deprecated use {@link #repairAsync(String keyspace, Map options)} instead.
+     *
+     * @param parallelismDegree 0: sequential, 1: parallel, 2: DC parallel
      * @return Repair command number, or 0 if nothing to repair
      */
-    public int forceRepairAsync(String keyspace, int parallelismDegree,
-            Collection<String> dataCenters, Collection<String> hosts,
-            boolean primaryRange, boolean fullRepair, String... columnFamilies);
-
-    public int forceRepairAsync(String keyspace);
-    /**
-     * Same as forceRepairAsync, but handles a specified range
-     */
-    public int forceRepairRangeAsync(String beginToken, String endToken,
-            String keyspaceName, boolean isSequential,
-            Collection<String> dataCenters, Collection<String> hosts,
-            boolean repairedAt, String... columnFamilies) throws IOException;
+    @Deprecated
+    public int forceRepairAsync(String keyspace, int parallelismDegree, Collection<String> dataCenters, Collection<String> hosts, boolean primaryRange, boolean fullRepair, String... tableNames);
 
     /**
-     * Same as forceRepairAsync, but handles a specified range
-     *
-     * @param parallelismDegree
-     *            0: sequential, 1: parallel, 2: DC parallel
+     * @deprecated use {@link #repairAsync(String keyspace, Map options)} instead.
      */
-    public int forceRepairRangeAsync(String beginToken, String endToken,
-            String keyspaceName, int parallelismDegree,
-            Collection<String> dataCenters, Collection<String> hosts,
-            boolean fullRepair, String... columnFamilies);
-
-    /**
-     * Invoke repair asynchronously. You can track repair progress by
-     * subscribing JMX notification sent from this StorageServiceMBean.
-     * Notification format is: type: "repair" userObject: int array of length 2,
-     * [0]=command number, [1]=ordinal of ActiveRepairService.Status
-     *
-     * @return Repair command number, or 0 if nothing to repair
-     */
-    public int forceRepairAsync(String keyspace, boolean isSequential,
-            boolean isLocal, boolean primaryRange, boolean fullRepair,
-            String... columnFamilies);
+    @Deprecated
+    public int forceRepairRangeAsync(String beginToken, String endToken, String keyspaceName, boolean isSequential, Collection<String> dataCenters, Collection<String> hosts, boolean fullRepair, String... tableNames) throws IOException;
 
     /**
      * Same as forceRepairAsync, but handles a specified range
+     *
+     * @deprecated use {@link #repairAsync(String keyspace, Map options)} instead.
+     *
+     * @param parallelismDegree 0: sequential, 1: parallel, 2: DC parallel
      */
-    public int forceRepairRangeAsync(String beginToken, String endToken,
-            String keyspaceName, boolean isSequential, boolean isLocal,
-            boolean repairedAt, String... columnFamilies);
+    @Deprecated
+    public int forceRepairRangeAsync(String beginToken, String endToken, String keyspaceName, int parallelismDegree, Collection<String> dataCenters, Collection<String> hosts, boolean fullRepair, String... tableNames);
+
+    /**
+     * @deprecated use {@link #repairAsync(String keyspace, Map options)} instead.
+     */
+    @Deprecated
+    public int forceRepairAsync(String keyspace, boolean isSequential, boolean isLocal, boolean primaryRange, boolean fullRepair, String... tableNames);
+
+    /**
+     * @deprecated use {@link #repairAsync(String keyspace, Map options)} instead.
+     */
+    @Deprecated
+    public int forceRepairRangeAsync(String beginToken, String endToken, String keyspaceName, boolean isSequential, boolean isLocal, boolean fullRepair, String... tableNames);
 
     public void forceTerminateAllRepairSessions();
 
@@ -497,6 +515,10 @@ public interface StorageServiceMBean extends NotificationEmitter {
 
     public List<String> getKeyspaces();
 
+    public List<String> getNonSystemKeyspaces();
+
+    public List<String> getNonLocalStrategyKeyspaces();
+
     /**
      * Change endpointsnitch class and dynamic-ness (and dynamic attributes) at
      * runtime
@@ -552,14 +574,12 @@ public interface StorageServiceMBean extends NotificationEmitter {
 
     public boolean isJoined();
 
-    @Deprecated
-    public int getExceptionCount();
-
     public void setStreamThroughputMbPerSec(int value);
 
     public int getStreamThroughputMbPerSec();
 
-    public int getCompactionThroughputMbPerSec();
+    public void setInterDCStreamThroughputMbPerSec(int value);
+    public int getInterDCStreamThroughputMbPerSec();
 
     public void setCompactionThroughputMbPerSec(int value);
 
@@ -635,7 +655,7 @@ public interface StorageServiceMBean extends NotificationEmitter {
     /**
      * Returns the configured tracing probability.
      */
-    public double getTracingProbability();
+    public double getTraceProbability();
 
     void disableAutoCompaction(String ks, String... columnFamilies)
             throws IOException;
@@ -663,8 +683,16 @@ public interface StorageServiceMBean extends NotificationEmitter {
     /** Sets the threshold for abandoning queries with many tombstones */
     public void setTombstoneFailureThreshold(int tombstoneDebugThreshold);
 
+    /** Returns the threshold for rejecting queries due to a large batch size */
+    public int getBatchSizeFailureThreshold();
+    /** Sets the threshold for rejecting queries due to a large batch size */
+    public void setBatchSizeFailureThreshold(int batchSizeDebugThreshold);
+
+    /** Sets the hinted handoff throttle in kb per second, per delivery thread. */
+    public void setHintedHandoffThrottleInKB(int throttleInKB);
+
     /**
      * Sets the hinted handoff throttle in kb per second, per delivery thread.
      */
-    public void setHintedHandoffThrottleInKB(int throttleInKB);
+    public boolean resumeBootstrap();
 }

commit 8176d5729f58edd93834edf826fe41750bcd3bc8

Bring back old forceKeyspaceCompaction() overload
nodetool from scylla-tools-java still uses it. Currently `nodetool compact` fails like this:

error: forceKeyspaceCompaction(java.lang.String, [Ljava.lang.String;)
-- StackTrace --
java.lang.NoSuchMethodException: forceKeyspaceCompaction(java.lang.String, [Ljava.lang.String;)

Fixes scylladb/scylla#2261

Probably broken by 3e146845b42cb6bafbcff336d70e3c846d5b1528

Message-Id: <1491470483-6147-1-git-send-email-tgrabiec@scylladb.com>

src/main/java/org/apache/cassandra/service/StorageService.java
@@ -582,6 +582,7 @@ public class StorageService extends MetricsMBean implements StorageServiceMBean,
     /**
      * Forces major compaction of a single keyspace
      */
+    @Override
     public void forceKeyspaceCompaction(String keyspaceName, String... columnFamilies)
             throws IOException, ExecutionException, InterruptedException {
         log(" forceKeyspaceCompaction(String keyspaceName, String... columnFamilies) throws IOException, ExecutionException, InterruptedException");

src/main/java/org/apache/cassandra/service/StorageServiceMBean.java
@@ -292,6 +292,12 @@ public interface StorageServiceMBean extends NotificationEmitter {
             throws IOException, ExecutionException, InterruptedException;
 
     /**
+     * Forces major compaction of a single keyspace.
+     */
+    public void forceKeyspaceCompaction(String keyspaceName, String... tableNames)
+            throws IOException, ExecutionException, InterruptedException;
+
+    /**
      * Trigger a cleanup of keys on a single keyspace
      */
     @Deprecated