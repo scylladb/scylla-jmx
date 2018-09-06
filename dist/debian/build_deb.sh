016b315e9144307fca7bb29615423ab33f656acd
Avi Kivity <avi@scylladb.com>
2018-09-06 18:55:09 +0300

    dist/debian: fix some pbuilder invocations
    pbuilder expects --configfile to come after the subcommand name; humor it.

--- dist/debian/build_deb.sh
+++ dist/debian/build_deb.sh
@@ -138,11 +138,11 @@ sudo -E DIST=$TARGET /usr/sbin/pbuilder update --configfile ./dist/debian/pbuild
 if [ "$TARGET" = "jessie" ]; then
     echo "apt-get install -y -t jessie-backports ca-certificates-java" > build/jessie-pkginst.sh
     chmod a+rx build/jessie-pkginst.sh
-    sudo -E DIST=$TARGET /usr/sbin/pbuilder --configfile ./dist/debian/pbuilderrc execute build/jessie-pkginst.sh
+    sudo -E DIST=$TARGET /usr/sbin/pbuilder execute --configfile ./dist/debian/pbuilderrc build/jessie-pkginst.sh
 elif [ "$TARGET" = "bionic" ]; then
     echo "apt-get install -y ca-certificates-java openjdk-8-jdk-headless" > build/bionic-workaround.sh
     echo "update-ca-certificates -f" >> build/bionic-workaround.sh
     chmod a+rx build/bionic-workaround.sh
-    sudo -E DIST=$TARGET /usr/sbin/pbuilder --configfile ./dist/debian/pbuilderrc execute --save-after-exec build/bionic-workaround.sh
+    sudo -E DIST=$TARGET /usr/sbin/pbuilder execute --configfile ./dist/debian/pbuilderrc --save-after-exec build/bionic-workaround.sh
 fi
 sudo -E DIST=$TARGET pdebuild --configfile ./dist/debian/pbuilderrc --buildresult build/debs
