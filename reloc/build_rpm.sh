7578d359af1263fc556bdea0f86ee1f0f0324e57
Pekka Enberg <penberg@scylladb.com>
2020-07-17 11:57:23 +0300

    reloc: Add "--builddir" option to build_{rpm,deb}.sh
    We need the ability to control build directory in scylla.git build
    system. Let's add support for the "--builddir" option like in other
    variants of the same scripts.
    
    Message-Id: <20200717085723.701209-1-penberg@scylladb.com>

--- reloc/build_rpm.sh
+++ reloc/build_rpm.sh
@@ -4,15 +4,21 @@
 print_usage() {
     echo "build_rpm.sh --reloc-pkg build/scylla-jmx-package.tar.gz"
     echo "  --reloc-pkg specify relocatable package path"
+    echo "  --builddir specify rpmbuild directory"
     exit 1
 }
 RELOC_PKG=$(readlink -f build/scylla-jmx-package.tar.gz)
+BUILDDIR=build/redhat
 OPTS=""
 while [ $# -gt 0 ]; do
     case "$1" in
         "--reloc-pkg")
             OPTS="$OPTS $1 $(readlink -f $2)"
             RELOC_PKG=$2
+            shift 2
+            ;;
+        "--builddir")
+            BUILDDIR="$2"
             shift 2
             ;;
         *)
@@ -24,7 +30,7 @@ done
 if [[ ! $OPTS =~ --reloc-pkg ]]; then
     OPTS="$OPTS --reloc-pkg $RELOC_PKG"
 fi
-mkdir -p build/redhat
-tar -C build/redhat/ -xpf $RELOC_PKG scylla-jmx/SCYLLA-RELEASE-FILE scylla-jmx/SCYLLA-RELOCATABLE-FILE scylla-jmx/SCYLLA-VERSION-FILE scylla-jmx/SCYLLA-PRODUCT-FILE scylla-jmx/dist/redhat
-cd build/redhat/scylla-jmx
+mkdir -p "$BUILDDIR"
+tar -C "$BUILDDIR" -xpf $RELOC_PKG scylla-jmx/SCYLLA-RELEASE-FILE scylla-jmx/SCYLLA-RELOCATABLE-FILE scylla-jmx/SCYLLA-VERSION-FILE scylla-jmx/SCYLLA-PRODUCT-FILE scylla-jmx/dist/redhat
+cd "$BUILDDIR"/scylla-jmx
 exec ./dist/redhat/build_rpm.sh $OPTS
