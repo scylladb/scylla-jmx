scylladb/scylla-jmx

commit da69e8b2c793af269c23154ee80372faa9955283

install.sh: detect java located under /usr/lib/jvm also
both fedora/centos and debian/ubuntu use a system to configure
the symbolic link of /usr/bin/java. the former uses `alternatives`
while the latter uses `update-alternatives`. if user installs multiple
packages provides the same tool, the system picks one using some
grading system. but the package provides java 8 or java 11 does not
always win, so there are good chances that `install.sh` either
fails to detect the java executable or it leaves us a symbolic link
which points to /usr/bin/java, which could be reassigned to
another java provided by a new JRE. and the new java might not be
compatible with JMX.

in this change, we always try to detect the java using the
packaging system, and use it if it is available and fall back
to /usr/bin/java.

this should address the issues, we recently ran into after
upgrading a fedora 37 system. the symptom was scylla-jmx
exited immediately after upgrading the system.

Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

install.sh
@@ -87,7 +87,28 @@ check_usermode_support() {
 
 if ! $packaging; then
     has_java=false
-    if [ -x /usr/bin/java ]; then
+    . /etc/os-release
+    case "$ID" in
+        ubuntu|debian)
+            for version in "8" "11"; do
+                java=$(dpkg -L openjdk-${version}-jre-headless | grep '/java$')
+                if [ -n "$java" ]; then
+                    break
+                fi
+            done
+            ;;
+        fedora|centos)
+            for version in "1.8.0" "11"; do
+                java=$(rpm -ql java-${version}-openjdk-headless | grep '/java$')
+                if [ -n "$java" ]; then
+                    break
+                fi
+            done
+            ;;
+    esac
+    if [ -n "$java" ]; then
+        has_java=true
+    elif [ -x /usr/bin/java ]; then
         javaver=$(/usr/bin/java -version 2>&1|head -n1|cut -f 3 -d " ")
         if [[ "$javaver" =~ ^\"1.8.0 || "$javaver" =~ ^\"11.0. ]]; then
             has_java=true
@@ -154,12 +175,12 @@ fi
 
 install -m644 scylla-jmx-1.0.jar "$rprefix/jmx"
 install -m755 scylla-jmx "$rprefix/jmx"
-ln -sf /usr/bin/java "$rprefix/jmx/symlinks/scylla-jmx"
+ln -sf "$java" "$rprefix/jmx/symlinks/scylla-jmx"
 if ! $nonroot; then
     install -m755 -d "$rusr"/lib/scylla/jmx/symlinks
     ln -srf "$rprefix"/jmx/scylla-jmx-1.0.jar "$rusr"/lib/scylla/jmx/
     ln -srf "$rprefix"/jmx/scylla-jmx "$rusr"/lib/scylla/jmx/
-    ln -sf /usr/bin/java "$rusr"/lib/scylla/jmx/symlinks/scylla-jmx
+    ln -sf "$java" "$rusr"/lib/scylla/jmx/symlinks/scylla-jmx
 fi
 
 if $nonroot; then

commit 8e77658dddf851fd3c7f030e06bd51de66cabf6f

install.sh: point java to /usr/bin/java
this should address the regression introduced by
da69e8b2c793af269c23154ee80372faa9955283, as we do pass --package
when cooking rpm and deb packages, in which case, $java is not set
after that offending commit. and before da69e8b2c7, $java defaults
to /usr/bin/java, let's restore that behavior.

Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

install.sh
@@ -85,6 +85,7 @@ check_usermode_support() {
     [ -n "$user" ]
 }
 
+java=/usr/bin/java
 if ! $packaging; then
     has_java=false
     . /etc/os-release